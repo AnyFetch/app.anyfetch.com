67815aad0948e6e0778ac23ff6eeca06cd31fd0a
Kim CHOUARD <kim.chouard@gmail.com>
2013-11-30 18:50:18 +0100

    ngRepeat on item result

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,17 +4,21 @@ angular.module('anyfetchFrontApp')
 	.controller('MainCtrl', function ($scope, $http) {
 
 		$scope.search = function () {
+			// DEBUG
+			$scope.userName = 'test@papiel.fr';
+			$scope.userPass = 'arf';
+
 			$scope.loading = true;
 
 			var searchUrl = 'http://api.anyfetch.com/documents?search='+$scope.textSearch+'&limit=50';
-			var basicAuthBase64 = btoa('test@papiel.fr:arf');
+			var basicAuthBase64 = btoa($scope.userName+':'+$scope.userPass);
 			console.log(basicAuthBase64);
 
 			$http.defaults.headers.common.Authorization = 'Basic ' + basicAuthBase64;
 			$http({method: 'GET', url: searchUrl})
 			.success(function(data, status, headers, config) {
-				console.log(data, status, headers, config);
-				$scope.datas = data.datas;
+				console.log(data, status, headers(), config);
+				$scope.results = data.datas;
 
 				$scope.loading = false;
 			})
