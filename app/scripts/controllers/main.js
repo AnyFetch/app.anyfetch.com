d99e7ce76119010f3a4c8ba46e5a19779a202f48
Kim CHOUARD <kim.chouard@gmail.com>
2013-12-03 23:11:56 +0100

    Search functionnal

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,32 +1,55 @@
 'use strict';
 
+// ------------------------------------------------------
+// 					MainCrtl
+// ------------------------------------------------------
+
 angular.module('anyfetchFrontApp')
 	.controller('MainCtrl', function ($scope, $http) {
 
-		$scope.search = function (query) {
-			// DEBUG
-			$scope.userName = 'test@papiel.fr';
-			$scope.userPass = 'arf';
+		// ----------------- Géneral functions -----------------
 
-			$scope.loading = true;
-
-			var searchUrl = 'http://api.anyfetch.com/documents?search='+query+'&limit=50';
+		$scope.apiCall = function(query, callback) {
+			var searchUrl = 'http://api.anyfetch.com/'+query;
 			var basicAuthBase64 = btoa($scope.userName+':'+$scope.userPass);
-			console.log(basicAuthBase64);
 
 			$http.defaults.headers.common.Authorization = 'Basic ' + basicAuthBase64;
 			$http({method: 'GET', url: searchUrl})
-			.success(function(data, status, headers, config) {
-				console.log(data, status, headers(), config);
-				$scope.results = data.datas;
-
-				$scope.loading = false;
-			})
+			.success(callback)
 			.error(function(data) {
 				console.log('Error ', data);
 			});
-
 		};
+
+		$scope.search = function (query) {
+			$scope.loading = true;
+
+			$scope.apiCall(
+				'documents?search='+query+'&limit=50',
+				function(data, status, headers, config) {
+					console.log(data, status, headers(), config);
+					$scope.results = data.datas;
+
+					$scope.loading = false;
+				});
+		};
+
+
+		// ----------------- Main -----------------
+
+		// DEBUG
+		$scope.userName = 'test@papiel.fr';
+		$scope.userPass = 'arf';
+
+		$scope.apiCall(
+			'',
+			function(data) {
+				console.log(data);
+
+				$scope.docTypes = data.document_types;
+				$scope.provStatus = data.provider_status;
+				$scope.userName = data.name;
+			});
 
 		$scope.search('style');
 	});
