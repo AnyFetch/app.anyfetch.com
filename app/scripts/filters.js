c76074bdf98f42e89a884af3431cf82798651ef6
Kim CHOUARD <kim.chouard@gmail.com>
2013-12-05 11:19:14 +0100

    clean up filters

--- app/scripts/filters.js
+++ app/scripts/filters.js
@@ -2,21 +2,19 @@
 
 /* Filters */
 
-var dateFilter = function(date, plus) {
-  date = new Date(date);
-
-  if (date) {
-    //Month in string
-    var month;
-    month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    var time = (plus) ? (' at ' + date.getHours() + ':' + date.getMinutes()) : '';
-    return month + ', ' + date.getDate() + time;
-  } else {
-    return '';
-  }
-};
-
 angular.module('anyfetchFrontApp.filters', []).
 filter('niceDate', function() {
-  return dateFilter;
+  return function(date, plus) {
+    date = new Date(date);
+
+    if (date) {
+      //Month in string
+      var month;
+      month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+      var time = (plus) ? (' at ' + date.getHours() + ':' + date.getMinutes()) : '';
+      return month + ', ' + date.getDate() + time;
+    } else {
+      return '';
+    }
+  };
 });
