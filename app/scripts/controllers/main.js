c6d427660bead8414e245c1d6a33e4d31932e8d3
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2013-12-12 14:45:58 +0100

    Update the search on redirection

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,14 +5,43 @@
 // ------------------------------------------------------
 
 angular.module('anyfetchFrontApp')
-.controller('MainCtrl', function ($scope, $location, AuthService) {
+.controller('MainCtrl', function ($scope, $location, $http, $q, AuthService) {
 
-  $scope.user = AuthService.currentUser;
+  $scope.search = function(query) {
+    $location.search({q: query});
+  }
   
+  $scope.getSnippets = function (query) {
+    var deferred = $q.defer();
+
+    var apiQuery = 'http://api.anyfetch.com/documents?search='+query+'&limit=50';
+    $scope.loading = true;
+
+    $http({method: 'GET', url: apiQuery})
+      .success(function(data) {
+        $scope.loading = false;
+        deferred.resolve(data);
+      })
+      .error(deferred.reject);
+
+    return deferred.promise;
+  };
+
   $scope.logout = function() {
     AuthService.logout(function() {
       $location.path('/login');
     });
+  };
+
+  $scope.user = AuthService.currentUser;
+  $scope.query  = $location.search().q || "";
+  $scope.results = [];
+
+  if ($scope.query) {
+    $scope.getSnippets($scope.query)
+      .then(function(data) {
+        console.log(data);
+      });
   };
 
 }).controller('LoginCtrl', function ($scope, $rootScope, $location, AuthService) {
