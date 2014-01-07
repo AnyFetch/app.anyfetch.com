cfb44b53e276e335482f8c114af5f558c69448c4
Kim CHOUARD <kim.chouard@gmail.com>
2014-01-07 17:51:24 +0100

    Load more button, 2 by 2
    check searching insa (:

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,21 +12,30 @@ angular.module('anyfetchFrontApp')
     $location.search({q: query});
   };
 
-  $scope.getSnippets = function (query) {
+  $scope.getRes = function (query, start, limit) {
     var deferred = $q.defer();
 
-    var apiQuery = 'http://api.anyfetch.com/documents?search='+query+'&limit=50';
+    var apiQuery = 'http://api.anyfetch.com/documents?search='+query+'&start='+start+'&limit='+limit;
 
     $http({method: 'GET', url: apiQuery})
       .success(function(data) {
         DocumentTypesService.updateSearchCounts(data.document_types);
         ProvidersService.updateSearchCounts(data.tokens);
         $scope.loading = false;
+        $scope.lastRes = start+limit;
+        $scope.moreResult = true;
         deferred.resolve(data);
       })
       .error(deferred.reject);
 
     return deferred.promise;
+  };
+
+  $scope.loadMore = function() {
+    $scope.getRes($scope.query, $scope.lastRes, 2)
+      .then(function(data) {
+        $scope.results = $scope.results.concat(data.datas);
+      });
   };
 
   $scope.logout = function() {
@@ -47,7 +56,7 @@ angular.module('anyfetchFrontApp')
   if ($scope.query) {
     $scope.loading = true;
 
-    $scope.getSnippets($scope.query)
+    $scope.getRes($scope.query, 0, 2)
       .then(function(data) {
         $scope.results = data.datas;
       });
