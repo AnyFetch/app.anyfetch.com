f937a660e5227ad580e48841343631d77a89cf15
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2014-02-12 11:27:56 +0100

    Fix of the Fix

--- app/scripts/services/authentication.js
+++ app/scripts/services/authentication.js
@@ -21,6 +21,11 @@ angular.module('anyfetchFrontApp.authenticationService', [])
     // Creation of the user credential
     var credentials;
     if (user) {
+      // IE8 & IE9 hack
+      if (!window.btoa) {
+        window.btoa = base64.encode;
+      }
+
       credentials = btoa(user.email + ':' + user.password);
     } else {
       credentials = $cookies.credentials;
