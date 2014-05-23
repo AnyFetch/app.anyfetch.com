f8793c96476721369179230641174b954867f02d
Kim CHOUARD <kim.chouard@gmail.com>
2014-05-23 16:43:01 +0200

    Import content in iFrame

--- app/scripts/directives/modal.js
+++ app/scripts/directives/modal.js
@@ -128,6 +128,9 @@ angular.module('anyfetchFrontApp.modalDirective', [])
           scope.fullText = Mustache.render(htmlTemplate, scope.documentfull.datas);
           scope.provider = ProvidersService.providers.list[scope.documentfull.token];
 
+          var iframe = ($('#iframe')[0].contentWindow || $('#test')[0].contentDocument);
+          iframe.document.body.innerHTML = scope.fullText;
+
           scope.bindEchap();
         }
       });
